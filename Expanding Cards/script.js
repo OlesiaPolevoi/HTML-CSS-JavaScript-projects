c91d628b9aa354b87578b77ff4da7518edaa1628
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-02-03 16:15:18 -0500

    finished JS portion

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("Expanding cards!!!");
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
