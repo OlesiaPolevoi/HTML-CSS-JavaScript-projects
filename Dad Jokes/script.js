9f98fb4398188eaf7d293b2bde9d6b49a2b42522
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-02-15 16:51:06 -0500

    dad jokes complete

---
+++ Dad Jokes/script.js
@@ -0,0 +1,36 @@
+"use strict";
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+//USING async await
+const generateJoke = async () => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const response = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await response.json();
+  jokeEl.innerHTML = data.joke;
+};
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+//USING .then
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+
+//   fetch("https://icanhazdadjoke.com", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
